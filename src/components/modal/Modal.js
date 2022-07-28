4a2d6a89e9c5c2d96c8fd78d6240fb51c1668ee4
eheath30 <elliot.heath@btinternet.com>
2022-07-28 10:23:13 +0100

    updating video function

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -6,13 +6,25 @@ const Modal = ({ songLyrics, setLyrics, songName, show, setShow }) => {
     setShow(false);
   }
 
+
+// change youtube url to embeded url for embedding
+let url= "https://www.youtube.com/watch?v=kXYiU_JCYtU"
+
+let embededURL = url.replace("watch?v=", "embed/")
+
+console.log(embededURL)
+
+
+
+
   return (
     <div id="myModal" className={`modal  ${show ? "showModal" : "hideModal"}`}>
       <div class="modal-content">
         <span onClick={closeModal} class="close">
           &times;
         </span>
-        <span>{songName}</span>
+        <span className="display-4 mx-auto">{songName}</span>
+        <iframe className="mx-auto my-4 rounded" width="560" height="315" src={embededURL} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         <div>{songLyrics}</div>
       </div>
     </div>
